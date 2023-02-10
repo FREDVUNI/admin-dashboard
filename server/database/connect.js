4016d2615ebf34e40354f0b03a19557e8a466995
FREDVUNI <fredvuni809@gmail.com>
2023-02-10 10:31:30 +0300

    switch mongo db connection from mongodb campass to atlas

--- server/database/connect.js
+++ server/database/connect.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDB = () =>{
     try{
         mongoose.set('strictQuery',false);
-        const con = mongoose.connect(process.env.MONGO_URL,{
+        const con = mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser:true,
             useUnifiedTopology:true,
             // useCreateIndex:true,
